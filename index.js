32292dd4ba440fd127aa52693897275dcf95179a
Tosan Makpa <tosanmakps@gmail.com>
2023-05-24 06:22:25 +0100

    first commit

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(session({
     store: MongoStore.create({
         mongoUrl: process.env.MONGODB_URI
     }),
-    cookie : {maxAge: new Date (Date.now() + (3600000))}
+   // cookie : {maxAge: new Date (Date.now() + (3600000))}
 
 }))
 
